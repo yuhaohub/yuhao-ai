85e02351a0a0950f9c455c510677a60424ee3512
hyh <hyh_hyh1@163.com>
2025-04-27 15:29:53 +0800

    init:初始化

---
+++ src/main/java/com/yuhao/yuhaoai/YuhaoAiApplication.java
@@ -0,0 +1,13 @@
+package com.yuhao.yuhaoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuhaoAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuhaoAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuhao/yuhaoai/YuhaoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhao.yuhaoai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuhaoAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
