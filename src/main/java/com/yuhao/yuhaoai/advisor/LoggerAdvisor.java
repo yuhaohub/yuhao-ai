2e8462e5126f46a48e802c49b96a9ec7a81d4072
hyh <hyh_hyh1@163.com>
2025-06-18 23:48:30 +0800

    feat:自定义advisor

---
+++ src/main/java/com/yuhao/yuhaoai/advisor/LoggerAdvisor.java
@@ -0,0 +1,46 @@
+package com.yuhao.yuhaoai.advisor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.advisor.api.*;
+import org.springframework.ai.chat.model.MessageAggregator;
+import reactor.core.publisher.Flux;
+
+public class LoggerAdvisor implements CallAroundAdvisor, StreamAroundAdvisor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerAdvisor.class);
+
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    @Override
+    public AdvisedResponse aroundCall(AdvisedRequest advisedRequest, CallAroundAdvisorChain chain) {
+
+        logger.info("BEFORE: {}", advisedRequest);
+
+        AdvisedResponse advisedResponse = chain.nextAroundCall(advisedRequest);
+
+        logger.info("AFTER: {}", advisedResponse);
+
+        return advisedResponse;
+    }
+
+    @Override
+    public Flux<AdvisedResponse> aroundStream(AdvisedRequest advisedRequest, StreamAroundAdvisorChain chain) {
+
+        logger.info("BEFORE: {}", advisedRequest);
+
+        Flux<AdvisedResponse> advisedResponses = chain.nextAroundStream(advisedRequest);
+
+        return new MessageAggregator().aggregateAdvisedResponse(advisedResponses,
+                advisedResponse -> logger.info("AFTER: {}", advisedResponse));
+    }
+
+
+}
